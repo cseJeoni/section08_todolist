73fc0662f658b52b8b3c442bcb110f7d385ce54b
Juyeon <s202155649@pusan.ac.kr>
2026-01-18 18:53:55 +0900

    8.3 ) 기능 구현 준비하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,42 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header'
-import Editor from './components/Editor'
-import List from './components/List'
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Editor from "./components/Editor";
+import List from "./components/List";
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime(),
+  },
+
+  {
+    id: 2,
+    isDone: false,
+    content: "노래부르기",
+    date: new Date().getTime(),
+  },
+];
 
 function App() {
-
+  const [todos, setTodos] = useState(mockData);
 
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <Editor />
       <List />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
