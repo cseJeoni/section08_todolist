fdeb895ed0fa902f7fb09582564daf020b5211d6
Juyeon <s202155649@pusan.ac.kr>
2026-01-21 20:36:31 +0900

    8.6) Update - 투두 수정하기

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,18 +1,36 @@
-import TodoItem from './TodoItem'
-import './List.css'
+import TodoItem from "./TodoItem";
+import "./List.css";
+import { useState } from "react";
 
-const List = () => {
+const List = ({ todos }) => {
+  const [search, setSearch] = useState("");
 
-    return (
-        <div className="List">
-            <h4>Todo List 🌱 </h4>
-        <input placeholder="검색어를 입력하세요"></input>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        </div>
-    )
-}
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === "") return todos;
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase())
+    );
+  };
+
+  const filteredTodos = getFilteredData();
+
+  return (
+    <div className="List">
+      <h4>Todo List 🌱 </h4>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      ></input>
+      {filteredTodos.map((todo) => {
+        return <TodoItem key={todo.id} {...todo} />;
+      })}
+    </div>
+  );
+};
 
 export default List;
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,16 +1,14 @@
-import './TodoItem.css'
+import "./TodoItem.css";
 
-
-const TodoItem = () => {
-
-    return (
-        <div className="TodoItem">
-            <input type="checkbox"></input>
-            <div className="content">React 공부하기</div>
-            <div className="date">2026.1.14</div>
-            <button>삭제</button>
-        </div>
-    )
-}
+const TodoItem = ({ id, isDone, content, date }) => {
+  return (
+    <div className="TodoItem">
+      <input readOnly checked={isDone} type="checkbox"></input>
+      <div className="content">{content}</div>
+      <div className="date">{date}</div>
+      <button>삭제</button>
+    </div>
+  );
+};
 
 export default TodoItem;
